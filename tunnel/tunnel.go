645c5c432f7c0f780bd93ab152fd852121524b60
wwqgtxx <wwqgtxx@gmail.com>
2025-05-27 18:28:11 +0800

    recover log leval for preHandleMetadata

--- tunnel/tunnel.go
+++ tunnel/tunnel.go
@@ -307,7 +307,7 @@ func handleUDPConn(packet C.PacketAdapter) {
 	}
 
 	if err := preHandleMetadata(metadata.Clone()); err != nil { // precheck without modify metadata
-		log.Warnln("[Metadata PreHandle] error: %s", err)
+		log.Debugln("[Metadata PreHandle] error: %s", err)
 		return
 	}
 
