6a752127f42a5e6795751e745da3d5256a3f5dc3
wwqgtxx <wwqgtxx@gmail.com>
2025-08-22 11:30:49 +0800

    rewrite vision client write

--- common/net/sing.go
+++ common/net/sing.go
@@ -24,6 +24,10 @@ type ExtendedReader = network.ExtendedReader
 
 var WriteBuffer = bufio.WriteBuffer
 
+type ReadWaitOptions = network.ReadWaitOptions
+
+var NewReadWaitOptions = network.NewReadWaitOptions
+
 func NewDeadlineConn(conn net.Conn) ExtendedConn {
 	if deadline.IsPipe(conn) || deadline.IsPipe(network.UnwrapReader(conn)) {
 		return NewExtendedConn(conn) // pipe always have correctly deadline implement
