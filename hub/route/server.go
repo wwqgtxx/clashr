81071a7a268dbd2dbb8aff66bfd6eb4f8480c290
wwqgtxx <wwqgtxx@gmail.com>
2025-12-03 11:34:37 +0800

    add `upTotal` and `downTotal` data to `/traffic` restful api

--- hub/route/server.go
+++ hub/route/server.go
@@ -58,8 +58,10 @@ var (
 )
 
 type Traffic struct {
-	Up   int64 `json:"up"`
-	Down int64 `json:"down"`
+	Up        int64 `json:"up"`
+	Down      int64 `json:"down"`
+	UpTotal   int64 `json:"upTotal"`
+	DownTotal int64 `json:"downTotal"`
 }
 
 type Memory struct {
@@ -437,9 +439,12 @@ func traffic(w http.ResponseWriter, r *http.Request) {
 	for range tick.C {
 		buf.Reset()
 		up, down := t.Now()
+		upTotal, downTotal := t.Total()
 		if err := json.NewEncoder(buf).Encode(Traffic{
-			Up:   up,
-			Down: down,
+			Up:        up,
+			Down:      down,
+			UpTotal:   upTotal,
+			DownTotal: downTotal,
 		}); err != nil {
 			break
 		}
