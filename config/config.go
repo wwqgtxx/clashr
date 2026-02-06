fb42f5f4e3a988a034b9d6d3ae79b7cc143a43a8
wwqgtxx <wwqgtxx@gmail.com>
2026-02-06 08:56:14 +0800

    support connection reuse for DoT

--- config/config.go
+++ config/config.go
@@ -999,7 +999,7 @@ func parseNameServer(servers []string, respectRules bool) ([]dns.NameServer, err
 			dnsNetType = "tcp" // TCP
 		case "tls":
 			addr, err = hostWithDefaultPort(u.Host, "853")
-			dnsNetType = "tcp-tls" // DNS over TLS
+			dnsNetType = "tls" // DNS over TLS
 		case "http", "https":
 			addr, err = hostWithDefaultPort(u.Host, "443")
 			dnsNetType = "https" // DNS over HTTPS
--- dns/util.go
+++ dns/util.go
@@ -91,6 +91,8 @@ func transform(servers []NameServer, resolver *Resolver) []dnsClient {
 	for _, s := range servers {
 		var c dnsClient
 		switch s.Net {
+		case "tls":
+			c = newDoTClient(s.Addr, resolver, s.Params, s.ProxyAdapter, s.ProxyName)
 		case "https":
 			c = newDoHClient(s.Addr, resolver, s.ProxyAdapter, s.ProxyName, s.Params)
 		case "dhcp":
