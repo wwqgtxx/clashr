ec2b1390fb2018203ed39e7c4d7c4136605fd6e6
wwqgtxx <wwqgtxx@gmail.com>
2026-03-21 15:10:10 +0800

    remove unreachable code

--- listener/sing_tun/server_android.go
+++ listener/sing_tun/server_android.go
@@ -3,49 +3,50 @@
 package sing_tun
 
 import (
+	"sync"
+
 	"github.com/metacubex/mihomo/log"
-	tun "github.com/metacubex/sing-tun"
-	"github.com/sagernet/netlink"
-	"golang.org/x/sys/unix"
-	"runtime"
+
+	"github.com/metacubex/sing-tun"
 )
 
-func (l *Listener) buildAndroidRules(tunOptions *tun.Options) error {
-	packageManager, err := tun.NewPackageManager(l.handler)
+type packageManagerCallback struct{}
+
+func (cb *packageManagerCallback) OnPackagesUpdated(packageCount int, sharedCount int) {}
+
+func newPackageManager() (tun.PackageManager, error) {
+	packageManager, err := tun.NewPackageManager(tun.PackageManagerOptions{
+		Callback: &packageManagerCallback{},
+		Logger:   log.SingLogger,
+	})
 	if err != nil {
-		return err
+		return nil, err
 	}
 	err = packageManager.Start()
 	if err != nil {
+		return nil, err
+	}
+	return packageManager, nil
+}
+
+var (
+	globalPM tun.PackageManager
+	pmOnce   sync.Once
+	pmErr    error
+)
+
+func getPackageManager() (tun.PackageManager, error) {
+	pmOnce.Do(func() {
+		globalPM, pmErr = newPackageManager()
+	})
+	return globalPM, pmErr
+}
+
+func (l *Listener) buildAndroidRules(tunOptions *tun.Options) error {
+	packageManager, err := getPackageManager()
+	if err != nil {
 		return err
 	}
-	l.packageManager = packageManager
 	tunOptions.BuildAndroidRules(packageManager, l.handler)
 	return nil
-}
-
-func (h *ListenerHandler) OnPackagesUpdated(packages int, sharedUsers int) {
-	return
-}
-
-func (l *Listener) openAndroidHotspot(tunOptions tun.Options) {
-	if runtime.GOOS == "android" && tunOptions.AutoRoute {
-		priority := 9000
-		if len(tunOptions.ExcludedRanges()) > 0 {
-			priority++
-		}
-		if tunOptions.InterfaceMonitor.AndroidVPNEnabled() {
-			priority++
-		}
-		it := netlink.NewRule()
-		it.Priority = priority
-		it.IifName = tunOptions.Name
-		it.Table = 254 //main
-		it.Family = unix.AF_INET
-		it.SuppressPrefixlen = 0
-		err := netlink.RuleAdd(it)
-		if err != nil {
-			log.Warnln("[TUN] add AndroidHotspot rule error")
-		}
-	}
 }
