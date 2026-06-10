a45ad01b85bbe766d250993c418c8fe7ec91aad1
wwqgtxx <wwqgtxx@gmail.com>
2026-06-10 14:36:44 +0800

    adjust internal function return type

--- adapter/outboundgroup/parser.go
+++ adapter/outboundgroup/parser.go
@@ -33,7 +33,7 @@ type GroupCommonOption struct {
 	Filter        string   `group:"filter,omitempty"`
 }
 
-func ParseProxyGroup(config map[string]any, proxyMap map[string]C.Proxy, providersMap map[string]P.ProxyProvider, healthCheckLazyDefault bool) (C.ProxyAdapter, error) {
+func ParseProxyGroup(config map[string]any, proxyMap map[string]C.Proxy, providersMap map[string]P.ProxyProvider, healthCheckLazyDefault bool) (ProxyGroup, error) {
 	decoder := structure.NewDecoder(structure.Option{TagName: "group", WeaklyTypedInput: true})
 
 	groupOption := &GroupCommonOption{
@@ -125,7 +125,7 @@ func ParseProxyGroup(config map[string]any, proxyMap map[string]C.Proxy, provide
 		groupOption.Filter = ""
 	}
 
-	var group C.ProxyAdapter
+	var group ProxyGroup
 	switch groupOption.Type {
 	case "url-test":
 		opts := parseURLTestOption(config)
